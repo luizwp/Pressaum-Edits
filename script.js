13035f0a1913330ce481275338af11b7e8a53ed2
Luiz Felipe <148246609+luizwp@users.noreply.github.com>
2024-08-20 21:46:36 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("enviar").addEventListener("click", function() {
+    // Exibe o overlay e o reloader ao clicar no botão "Enviar"
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("reloader").style.display = "block";
+});
